3016aa43ba1fb0d8df5399268412868beafc88fe
Phat <phat.le@hotmail.com>
2020-02-07 14:48:36 -0800

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+/** Implicit Return instead of writing
+ * () => {
+ * return (...);
+ * }
+ */
 
-export default App;
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <p>Hello World</p>
+    </header>
+  </div>
+);
